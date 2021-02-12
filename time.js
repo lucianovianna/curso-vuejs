66d912342114bfc8608419c9b053e92d4a871eb4
lucianovianna <luciano11v11@gmail.com>
2021-02-12 15:41:44 -0300

    Continuando de onde o curso anterior parou

---
+++ time.js
@@ -0,0 +1,53 @@
+class Time {
+    constructor(nome, escudo) {
+        this.nome = nome;
+        this.escudo = escudo;
+
+        this.pontos = 0;
+        this.gm = 0;
+        this.gs = 0;
+        this.saldo = 0;
+    }
+
+    fimJogo(timeVisitante, golsMarcados, golsSofridos) {
+        if(this.foiEmpate(golsMarcados, golsSofridos)) {
+            this.empate(golsMarcados, golsSofridos);
+            timeVisitante.empate(golsSofridos, golsMarcados); // para o time visitante os gols sofridos foram os gols q ele marcou.
+        }
+        else if(this.foiVitoria(golsMarcados, golsSofridos)) {
+            this.vitoria(golsMarcados, golsSofridos);
+            timeVisitante.derrota(golsSofridos, golsMarcados);
+        }
+        else {
+            this.derrota(golsMarcados, golsSofridos);
+            timeVisitante.vitoria(golsSofridos, golsMarcados);
+        }
+    }
+
+    foiEmpate(golsMarcados, golsSofridos) {
+        return golsMarcados === golsSofridos;
+    }
+
+    foiVitoria(golsMarcados, golsSofridos) {
+        return golsMarcados > golsSofridos;
+    }
+
+    empate(golsMarcados, golsSofridos) {
+        this.atualizarInfo(1, golsMarcados, golsSofridos);
+    }
+
+    vitoria(golsMarcados, golsSofridos) {
+        this.atualizarInfo(3, golsMarcados, golsSofridos);
+    }
+
+    derrota(golsMarcados, golsSofridos) {
+        this.atualizarInfo(0, golsMarcados, golsSofridos);
+    }
+
+    atualizarInfo(pontos, golsMarcados, golsSofridos) {
+        this.pontos += pontos;
+        this.gm += golsMarcados;
+        this.gs += golsSofridos;
+        this.saldo = this.gm - this.gs;
+    }
+}
