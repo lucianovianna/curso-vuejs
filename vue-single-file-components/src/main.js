fa6386a40c8db597ea4ac72f21e23212d972326e
lucianovianna <luciano11v11@gmail.com>
2021-02-18 13:26:52 -0300

    Componentizando o component 'titulo'

--- vue-single-file-components/src/main.js
+++ vue-single-file-components/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Titulo from './components/Titulo.vue'
 
 Vue.config.productionTip = false
+
+Vue.component('titulo', Titulo);
 
 new Vue({
   render: h => h(App),
