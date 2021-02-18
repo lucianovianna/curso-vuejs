9ea124c1e38efc21e15f0cda5071b168f14e0f50
lucianovianna <luciano11v11@gmail.com>
2021-02-18 13:45:40 -0300

    criando TabelaClubes.vue criando NovoJogo.vue

--- vue-single-file-components/src/main.js
+++ vue-single-file-components/src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Titulo from './components/Titulo.vue'
+import NovoJogo from './components/NovoJogo.vue'
+import TabelaClubes from './components/TabelaClubes.vue'
 
 Vue.config.productionTip = false
 
 Vue.component('titulo', Titulo);
+Vue.component('novo-jogo', NovoJogo);
+Vue.component('tabela-clubes', TabelaClubes);
 
 new Vue({
   render: h => h(App),
