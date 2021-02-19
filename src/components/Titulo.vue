79f7971d828d60622fd252ff82e00118e6218cd9
lucianovianna <luciano11v11@gmail.com>
2021-02-19 14:59:28 -0300

    Add functions do life cicle no component 'titulo'

--- src/components/Titulo.vue
+++ src/components/Titulo.vue
@@ -6,6 +6,36 @@
 
 <script>
 export default {
+  beforeCreate() {
+    //criar coisa no this
+    //verificar se o usuario está autenticado pra definir configuracoes
+    //verificar informacoes no localstorage
+    console.log(Object.assign({}, this), "beforeCreate");
+  },
+  created() {
+    //this.variavel = new Date();
+    console.log(Object.assign({}, this), "create");
+  },
+  beforeMount() {
+    //this.$attrs['']
+    // aqui já tem attrs
+    console.log(Object.assign({}, this), "beforeMount");
+  },
+  mounted() {
+    console.log(Object.assign({}, this), "mounted");
+  },
+  beforeUpdate() {
+    console.log(Object.assign({}, this), "beforeUpdate");
+  },
+  updated() {
+    console.log(Object.assign({}, this), "updated");
+  },
+  beforeDestroy() {
+    console.log(Object.assign({}, this), "beforeDestroy");
+  },
+  destroyed() {
+    console.log(Object.assign({}, this), "destroyed");
+  },
   methods: {
     clicke() {
       console.log(this.$parent.visao);
